2cd794569b413026546550cc273896591796eb71
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2019-08-10 15:30:31 -0400

    Add bro_name And movie_title To Review Resource

--- app/resources/review_resource.rb
+++ app/resources/review_resource.rb
@@ -1,9 +1,17 @@
 class ReviewResource < JSONAPI::Resource
   immutable
 
-  attributes :description, :grade
+  attributes :description, :grade, :bro_name, :movie_title
   has_one :movie
   has_one :bro
 
   filters :movie, :bro
+
+  def bro_name
+    @model.bro.name
+  end
+
+  def movie_title
+    @model.movie.title
+  end
 end
